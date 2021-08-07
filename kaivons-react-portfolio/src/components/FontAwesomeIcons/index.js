56e36396403c4dacb866495cc6344814e0b5a6cd
kaivon tolooee <kaivont@gmail.com>
2021-08-07 16:39:41 -0400

    trying to get fontawesome to work

---
+++ kaivons-react-portfolio/src/components/FontAwesomeIcons/index.js
@@ -0,0 +1,4 @@
+import { library } from '@fortawesome/fontawesome-svg-core';
+import {fa-github-square, fa-linkedin, fa-envelope-square, fa-instagram } from '@fortawesome/free-solid-svg-icons';
+
+library.add(fa-github-square, fa-linkedin, fa-envelope-square, fa-instagram);
--- kaivons-react-portfolio/src/components/Navbar.js
+++ kaivons-react-portfolio/src/components/Navbar.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import './FontAwesomeIcons/index'
+
 
 
 
@@ -8,7 +11,7 @@ export default class Navbar extends React.Component{
     return (
       
       <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand">Home</a>
+  {/* <a class="navbar-brand">Home</a> */}
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
@@ -18,9 +21,12 @@ export default class Navbar extends React.Component{
       <a class="nav-item nav-link" href="#About">About Me</a>
       <a class="nav-item nav-link" href="#Work">My Projects</a>
       <a class="nav-item nav-link" href="#Contact">Contact Me</a>
-      <a class="nav-item nav-link" href="#">Disabled</a>
-      <a class="nav-item nav-link" href="#">Disabled</a>
-      <a class="nav-item nav-link active" href="https://github.com/kaivont85"><i class="fab fa-github-square"></i><span class="sr-only">(current)</span></a> 
+      {/* <a class="nav-item nav-link" href="#">Disabled</a>
+      <a class="nav-item nav-link" href="#">Disabled</a> */}
+      <a class="nav-item nav-link active" href="https://github.com/kaivont85">
+        fa-github-square
+        <FontAwesomeIcon icon="github-square" />
+        <span class="sr-only">(current)</span></a> 
     </div>
   </div>
 </nav>
